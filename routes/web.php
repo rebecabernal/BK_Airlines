524db8cee8c0909cf0187e68c1787f4a69b8726e
rebecabernal <rebecabernalmesa@gmail.com>
2025-01-31 18:48:04 +0100

    build: middleware structure

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Container\Attributes\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
